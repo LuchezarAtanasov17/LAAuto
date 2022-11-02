cd15aa58b1762a3357efc8ca129029e452abcc8e
Luchezar_a <lachezaratanasov1705@gmail.com>
2022-11-02 22:06:55 +0200

    Add project files.

---
+++ LAAuto.web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LAAuto.web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
