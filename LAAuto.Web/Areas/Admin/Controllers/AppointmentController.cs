9c196f9660194348c67e6fdbb8ca35d616c8be82
Luchezar_a <lachezaratanasov1705@gmail.com>
2022-12-12 01:44:13 +0200

    Add appointments page for admin.

---
+++ LAAuto.Web/Areas/Admin/Controllers/AppointmentController.cs
@@ -0,0 +1,37 @@
+﻿using LAAuto.Web.Models.Appointments;
+using Microsoft.AspNetCore.Mvc;
+using LAAuto.Services.Appointments;
+
+namespace LAAuto.Web.Areas.Admin.Controllers
+{
+    public class AppointmentController : BaseController
+    {
+        private readonly IAppointmentService _appointmentService;
+
+        public AppointmentController(IAppointmentService appointmentService)
+        {
+            _appointmentService = appointmentService ?? throw new ArgumentNullException(nameof(appointmentService));
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> List()
+        {
+            var serviceAppointments = await _appointmentService.ListAppointmentsAsync();
+
+            var appointments = serviceAppointments
+                .Select(Conversion.ConvertAppointment)
+                .ToList();
+
+            return View(appointments);
+        }
+
+        public async Task<IActionResult> Delete(
+            [FromRoute]
+            Guid id)
+        {
+            await _appointmentService.DeleteAppointmentAsync(id);
+
+            return RedirectToAction(nameof(List));
+        }
+    }
+}
